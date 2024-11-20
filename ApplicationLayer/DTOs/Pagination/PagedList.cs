b8200a1cd72a43d9538b0a39446986db2024078d
Le Thanh Trung <trunglt1011@gmail.com>
2024-11-20 22:47:53 +0700

    Pagination

--- ApplicationLayer/DTOs/Pagination/PagedList.cs
+++ ApplicationLayer/DTOs/Pagination/PagedList.cs
@@ -1,28 +1,11 @@
 ﻿namespace ApplicationLayer.DTOs.Pagination {
-	public partial class PagedList<T> : List<T>, IPagedList<T> {
-		public PagedList(IList<T> source, int pageIndex, int pageSize, int? totalCount = null) {
-			pageSize = Math.Max(pageSize, 1);
-
-			TotalCount = totalCount ?? source.Count;
-			TotalPages = TotalCount / pageSize;
-
-			if (TotalCount % pageSize > 0) TotalPages++;
-
+	public class PagedList<T> : PagedResultBase {
+		public IList<T> Items { get; set; } = new List<T>();
+		public PagedList(IList<T> items, int pageNumber, int pageSize, int count) {
+			TotalRecord = count;
+			Items = items;
 			PageSize = pageSize;
-			PageIndex = pageIndex;
-			AddRange(totalCount != null ? source : source.Skip(PageIndex + 1).Take(pageSize));
+			PageNumber = pageNumber;
 		}
-
-		public int PageIndex { get; set; }
-
-		public int PageSize { get; set; }
-
-		public int TotalCount { get; set; }
-
-		public int TotalPages { get; set; }
-
-		public bool HasPreviousPage => PageIndex > 0;
-
-		public bool HasNextPage => PageIndex + 1 < TotalPages;
 	}
 }
